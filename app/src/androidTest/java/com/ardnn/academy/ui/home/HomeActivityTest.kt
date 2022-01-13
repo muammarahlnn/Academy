a58d889d34624d0089a61903df843c17afb49113
muammarahlnn <21muammarahlan@gmail.com>
2022-01-14 00:56:44 +0800

    make UI test work for livedata

--- app/src/androidTest/java/com/ardnn/academy/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/com/ardnn/academy/ui/home/HomeActivityTest.kt
@@ -17,26 +17,23 @@ import org.junit.Test
 class HomeActivityTest {
 
     private val dummyCourses = DataDummy.generateDummyCourses()
-    private val dummyBookmarks = DataDummy.generateDummyBookmarks()
 
-//    @get:Rule
-//    var activityRule = ActivityScenarioRule(HomeActivity::class.java)
-
-    @Before
-    fun setUp() {
-        ActivityScenario.launch(HomeActivity::class.java)
-    }
+    @get:Rule
+    var activityRule = ActivityScenarioRule(HomeActivity::class.java)
 
     @Test
     fun loadCourses() {
+        delayTwoSecond()
         onView(withId(R.id.rv_academy)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyCourses.size))
     }
 
     @Test
     fun loadDetailCourse() {
+        delayTwoSecond()
         onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
 
+        delayTwoSecond()
         onView(withId(R.id.text_title)).check(matches(isDisplayed()))
         onView(withId(R.id.text_title)).check(matches(withText(dummyCourses[0].title)))
 
@@ -46,23 +43,46 @@ class HomeActivityTest {
 
     @Test
     fun loadModule() {
+        delayTwoSecond()
         onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+
+        delayTwoSecond()
         onView(withId(R.id.btn_start)).perform(click())
+
+        delayTwoSecond()
         onView(withId(R.id.rv_module)).check(matches(isDisplayed()))
     }
 
     @Test
     fun loadDetailModule() {
+        delayTwoSecond()
         onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+
+        delayTwoSecond()
         onView(withId(R.id.btn_start)).perform(click())
+
+        delayTwoSecond()
         onView(withId(R.id.rv_module)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+
+        delayTwoSecond()
         onView(withId(R.id.web_view)).check(matches(isDisplayed()))
     }
 
     @Test
     fun loadBookmarks() {
         onView(withText("Bookmark")).perform(click())
+
+        delayTwoSecond()
         onView(withId(R.id.rv_bookmark)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_bookmark)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyBookmarks.size))
+        onView(withId(R.id.rv_bookmark)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyCourses.size))
+    }
+
+    private fun delayTwoSecond() {
+        try {
+            // add 0.1 second bcs there is phone that couldn't load all the data from remote source in exactly 2 seconds
+            Thread.sleep(2100)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
     }
 }
