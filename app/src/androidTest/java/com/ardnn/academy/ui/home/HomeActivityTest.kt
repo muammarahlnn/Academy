c5682e7a2397e1dd8a1fb7e30c507e743c905679
muammarahlnn <21muammarahlan@gmail.com>
2022-01-14 18:28:30 +0800

    add idling resources

--- app/src/androidTest/java/com/ardnn/academy/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/com/ardnn/academy/ui/home/HomeActivityTest.kt
@@ -3,6 +3,7 @@ package com.ardnn.academy.ui.home
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
@@ -10,6 +11,8 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.ardnn.academy.R
 import com.ardnn.academy.utils.DataDummy
+import com.ardnn.academy.utils.EspressoIdlingResource
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -21,68 +24,50 @@ class HomeActivityTest {
     @get:Rule
     var activityRule = ActivityScenarioRule(HomeActivity::class.java)
 
+    @Before
+    fun setUp() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.idlingResource)
+    }
+
+    @After
+    fun tearDown() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.idlingResource)
+    }
+
     @Test
     fun loadCourses() {
-        delayTwoSecond()
         onView(withId(R.id.rv_academy)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyCourses.size))
     }
 
     @Test
     fun loadDetailCourse() {
-        delayTwoSecond()
         onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-
-        delayTwoSecond()
         onView(withId(R.id.text_title)).check(matches(isDisplayed()))
         onView(withId(R.id.text_title)).check(matches(withText(dummyCourses[0].title)))
-
         onView(withId(R.id.text_date)).check(matches(isDisplayed()))
         onView(withId(R.id.text_date)).check(matches(withText("Deadline ${dummyCourses[0].deadline}")))
     }
 
     @Test
     fun loadModule() {
-        delayTwoSecond()
         onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-
-        delayTwoSecond()
         onView(withId(R.id.btn_start)).perform(click())
-
-        delayTwoSecond()
         onView(withId(R.id.rv_module)).check(matches(isDisplayed()))
     }
 
     @Test
     fun loadDetailModule() {
-        delayTwoSecond()
         onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-
-        delayTwoSecond()
         onView(withId(R.id.btn_start)).perform(click())
-
-        delayTwoSecond()
         onView(withId(R.id.rv_module)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-
-        delayTwoSecond()
         onView(withId(R.id.web_view)).check(matches(isDisplayed()))
     }
 
     @Test
     fun loadBookmarks() {
         onView(withText("Bookmark")).perform(click())
-
-        delayTwoSecond()
         onView(withId(R.id.rv_bookmark)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_bookmark)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyCourses.size))
-    }
-
-    private fun delayTwoSecond() {
-        try {
-            // add 0.1 second bcs there is phone that couldn't load all the data from remote source in exactly 2 seconds
-            Thread.sleep(2100)
-        } catch (e: InterruptedException) {
-            e.printStackTrace()
-        }
     }
 }
--- app/src/main/java/com/ardnn/academy/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/ardnn/academy/data/source/remote/RemoteDataSource.kt
@@ -5,6 +5,7 @@ import android.os.Looper
 import com.ardnn.academy.data.source.remote.response.ContentResponse
 import com.ardnn.academy.data.source.remote.response.CourseResponse
 import com.ardnn.academy.data.source.remote.response.ModuleResponse
+import com.ardnn.academy.utils.EspressoIdlingResource
 import com.ardnn.academy.utils.JsonHelper
 
 class RemoteDataSource private constructor(private val jsonHelper: JsonHelper) {
@@ -30,20 +31,26 @@ class RemoteDataSource private constructor(private val jsonHelper: JsonHelper) {
     }
 
     fun getAllCourses(callback: LoadCoursesCallback) {
+        EspressoIdlingResource.increment()
         handler.postDelayed({
             callback.onAllCoursesReceived(jsonHelper.loadCourses())
+            EspressoIdlingResource.decrement()
         }, SERVICE_LATENCY_IN_MILLIS)
     }
 
     fun getModules(courseId: String, callback: LoadModulesCallback) {
+        EspressoIdlingResource.increment()
         handler.postDelayed({
             callback.onAllModulesReceived(jsonHelper.loadModule(courseId))
+            EspressoIdlingResource.decrement()
         }, SERVICE_LATENCY_IN_MILLIS)
     }
 
     fun getContent(moduleId: String, callback: LoadContentCallback) {
+        EspressoIdlingResource.increment()
         handler.postDelayed({
             callback.onContentReceived(jsonHelper.loadContent(moduleId))
+            EspressoIdlingResource.decrement()
         }, SERVICE_LATENCY_IN_MILLIS)
     }
 
